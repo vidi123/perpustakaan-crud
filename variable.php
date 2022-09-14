72bf26bd224cb8c3a703e72095f5725f15251b4f
vidi123 <fadil.arofah79@gmail.com>
2022-09-14 09:23:24 +0700

    menambahkan tombol kembali pada tiap halaman

---
+++ variable.php
@@ -0,0 +1,11 @@
+<?php
+function variable() {
+    $db_function[0] = "localhost";
+    $db_function[1] = "root";
+    $db_function[2] = "";
+    $db_function[3] = "library_db";
+    return $db_function;
+}
+
+
+?>
